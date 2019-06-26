227f0f3bcc4f5bafb6d63eb574090c973350f31a
WMXPY <wm@mengw.io>
2019-06-26 09:45:19 -0500

    init objects

---
+++ src/loading.tsx
@@ -0,0 +1,83 @@
+/**
+ * @author WMXPY
+ * @namespace BWNL
+ * @description Spinner
+ */
+
+import { Classes } from "jss";
+import * as React from "react";
+import { assertIfTrue, mergeClasses } from "./style/decorator";
+import { LoadingStyle } from "./style/loading";
+
+export type NeonSpinnerProps = {
+
+    readonly loading: boolean;
+};
+
+export type NeonSpinnerState = {
+
+    readonly spinning: boolean;
+};
+
+export class NeonSpinnerBase extends React.Component<NeonSpinnerProps, NeonSpinnerState> {
+
+    public readonly state: NeonSpinnerState = {
+
+        spinning: this.props.loading,
+    };
+
+    private _timer: any;
+    private readonly _loadingStyle: Classes = LoadingStyle.use();
+
+    public constructor(props: NeonSpinnerProps) {
+
+        super(props);
+
+        this._timer = undefined;
+    }
+
+    public componentWillReceiveProps(nextProps: NeonSpinnerProps) {
+
+        clearTimeout(this._timer);
+
+        if (nextProps.loading) {
+
+            this.setState({
+                spinning: true,
+            });
+        } else {
+
+            this._timer = setTimeout(() => this.setState({
+                spinning: false,
+            }), 1000);
+        }
+    }
+
+    public render(): JSX.Element {
+
+        return (<div className={this._loadingStyle.loading}>
+            <div className={this._loadingStyle.outer} >
+                <div className={this._frontClass()} />
+                <div className={this._backClass()} />
+            </div>
+        </div>);
+    }
+
+    private _frontClass(): string | undefined {
+
+        return mergeClasses(
+            this._loadingStyle.front,
+            this.props.loading ? this._loadingStyle.enableFront : this._loadingStyle.disable,
+            assertIfTrue(this.state.spinning, this._loadingStyle.spinningFront),
+        );
+    }
+
+    private _backClass(): string | undefined {
+
+        return mergeClasses(
+            this._loadingStyle.back,
+            this.props.loading ? this._loadingStyle.enableBack : this._loadingStyle.disable,
+            assertIfTrue(this.state.spinning, this._loadingStyle.spinningBack),
+        );
+    }
+}
---
+++ src/style/loading.ts
@@ -0,0 +1,69 @@
+/**
+ * @author WMXPY
+ * @namespace Style
+ * @description Loading
+ */
+
+import { JSSStyle } from "./decorator";
+import { StyleManager } from "./style";
+
+const commonFrontBackStyle = {
+
+    transition: '0.8s all ease-in-out',
+    pointerEvents: 'none',
+    position: 'absolute',
+    width: '3rem',
+    height: '3rem',
+    opacity: 1,
+
+    '&.disable': {
+        border: '0px solid red',
+    },
+};
+
+const keyframes = {
+
+    '100%': 'transform: rotate(360deg)',
+};
+
+const LoadingStyleBase: JSSStyle = {
+
+    loading: {
+        position: 'relative',
+        transition: '0.5s all ease-in-out',
+        margin: '1.5rem',
+        width: '3rem',
+        height: '3rem',
+    },
+    disable: {
+        outline: `0.8rem solid transparent`,
+    },
+    enableFront: {
+        outline: `0.5rem solid #001F3F`,
+    },
+    enableBack: {
+        outline: `0.5rem solid #01FF70`,
+    },
+    outer: {
+        transition: '0.5s all ease-in-out',
+        position: 'absolute',
+    },
+    front: {
+        zIndex: 1,
+        ...commonFrontBackStyle,
+    },
+    back: {
+        ...commonFrontBackStyle,
+    },
+    spinningFront: {
+        animation: 'bwnl-loading-rotate 3s infinite',
+    },
+    spinningBack: {
+        animation: 'bwnl-loading-rotate 1.5s infinite',
+    },
+    '@global': {
+        '@keyframes bwnl-loading-rotate': keyframes,
+    },
+};
+
+export const LoadingStyle: StyleManager = StyleManager.create(LoadingStyleBase, 'Loading');
