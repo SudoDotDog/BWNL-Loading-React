abe8263fbdbebe34a5d767a11c85469d9ddbee2a
WMXPY <wm@mengw.io>
2020-07-11 19:14:32 -0500

    add loading knobs to view.story

--- stories/view.story.tsx
+++ stories/view.story.tsx
@@ -5,17 +5,21 @@
  * @override Story
  */
 
+import { boolean, withKnobs } from "@storybook/addon-knobs";
 import * as React from 'react';
 import { LoadingView } from "../src";
 
 export default {
     title: 'Loading View',
+    decorators: [withKnobs],
 };
 
 export const Simple = () => {
 
+    const loading: boolean = boolean('loading', true);
+
     return (<LoadingView
-        loading={true}
+        loading={loading}
         width={300}
         height={300}
     >
